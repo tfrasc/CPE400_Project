68b79847dfc8ff5714af84e0bed1274397da3ba0
Tyler Frasca <tylerfrasca@Elevates-MacBook-Pro.local>
2016-12-03 01:57:59 -0800

    Added setting and resetting status of connections

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,17 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def home
-    
+    @status1 = Connection.find(1)
+    @status2 = Connection.find(2)
+  end
+
+  def update
+    @connection = Connection.find(params[:id])
+    @connection.update(status: true)
+  end
+
+  def reset
+    @connection = Connection.find(params[:id])
+    @connection.update(status: false)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/', to: 'application#home'
+  post '/update', to: 'application#update'
+  post '/reset', to: 'application#reset'
 end
